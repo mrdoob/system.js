30b7a9eb4cc5d0a827969f6a2b97ee6816ca14ef
Mr.doob <info@mrdoob.com>
2012-09-29 20:18:56 +0200

    Added build system.

---
+++ utils/builder.py
@@ -0,0 +1,9 @@
+import os
+
+source = '../src/System.js'
+output = '../build/system.min.js'
+
+os.system('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + output)
+
+with open(output,'r') as f: text = f.read()
+with open(output,'w') as f: f.write("// system.js - http://github.com/mrdoob/system.js\n" + text)
