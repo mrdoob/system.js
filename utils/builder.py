8e94fe5e2bff39aa09c149aed78c54431369ca8e
Mr.doob <info@mrdoob.com>
2012-10-27 11:12:28 +0200

    Compiling to ECMASCRIPT5_STRICT.

--- utils/builder.py
+++ utils/builder.py
@@ -3,7 +3,7 @@ import os
 source = '../src/System.js'
 output = '../build/system.min.js'
 
-os.system('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + output)
+os.system('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5_STRICT --js ' + source + ' --js_output_file ' + output)
 
 with open(output,'r') as f: text = f.read()
 with open(output,'w') as f: f.write("// system.js - http://github.com/mrdoob/system.js\n" + text)
